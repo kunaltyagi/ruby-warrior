675bff98ec3b45da04d52f906ff0ea3f6d91f23d
Kunal Tyagi <tyagi.kunal@live.com>
2014-02-05 10:15:08 +0530

    Create level_seven.rb

---
+++ level_seven.rb
@@ -0,0 +1,54 @@
+class Player
+ 
+  def play_turn(warrior)
+    @max_health = 20
+    @min_health = 10
+    if @health == nil then @health = @max_health end
+    is_taking_damage?(warrior)
+    actions(warrior)
+    record_health(warrior)
+  end
+ 
+  def is_taking_damage?(warrior)
+    #checks current health vs previous health to see if we are being attacked
+    @under_attack = @health > warrior.health
+  end
+ 
+  def actions(warrior) 
+  #potential actions the warrior can take to respond to situations
+    if warrior.feel.wall? == true
+      warrior.pivot!
+    elsif @wall != 1
+      if warrior.feel(:backward).empty? == true
+        warrior.walk!(:backward)
+      elsif warrior.feel(:backward).enemy? == true
+        warrior.attack!(:backward)
+      elsif warrior.feel(:backward).captive? == true
+        warrior.rescue!(:backward)
+      elsif warrior.feel(:backward).wall? == true
+        @wall = 1
+      end
+    elsif warrior.feel.enemy? == true
+      warrior.attack!
+    elsif warrior.feel.captive? ==true
+      warrior.rescue!
+    elsif warrior.feel.empty? == true
+      if @under_attack == true
+        if warrior.health < @min_health
+          warrior.walk!(:backward)
+        else
+          warrior.walk!
+        end
+      elsif warrior.health >= @max_health
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+    end
+ end
+ 
+  def record_health(warrior)
+    @health = warrior.health
+  end
+ 
+end
