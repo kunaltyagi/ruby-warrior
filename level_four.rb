3ce18f4a5091126f873179ac0d2c23a0f652dccd
Kunal Tyagi <tyagi.kunal@live.com>
2014-02-05 10:06:20 +0530

    Create level_four.rb

---
+++ level_four.rb
@@ -0,0 +1,43 @@
+class Player
+ 
+  def play_turn(warrior)
+    @max_health = 20
+    @min_health = 7
+    @rest_gain  = 2
+    if @health == nil then @health = @max_health end
+    is_taking_damage?(warrior)
+    actions(warrior)
+    record_health(warrior)
+  end
+ 
+  def is_taking_damage?(warrior)
+    #checks current health vs previous health to see if we are being attacked
+    @under_attack = @health > warrior.health
+  end
+ 
+  def actions(warrior) 
+  #potential actions the warrior can take to respond to situations
+  if warrior.feel.enemy? == true
+      warrior.attack!
+    elsif warrior.feel.captive? ==true
+      warrior.rescue!
+    elsif warrior.feel.empty? == true
+      if @under_attack == true
+        if warrior.health < @min_health
+          warrior.walk!(:backward)
+        else
+          warrior.walk!
+        end
+      elsif warrior.health >= @max_health
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+    end
+ end
+ 
+  def record_health(warrior)
+    @health = warrior.health
+  end
+ 
+end
